559f07ba61c7889374c016a613ba03b0e5fb9772
Gorgees Odisho <gorgees041@gmail.com>
2023-10-15 08:53:14 +0300

    PROPS-MINI-PROJECT DONE + BUNOS

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,22 @@
-function Article({title, date="January 1, 1970", preview}) {
+function Article({title, date="January 1, 1970", preview, time}) {
+    
+    let rest = ""
+    if (time <= 5) {
+        rest = "☕️"
+    } else if (time > 5 && time < 30) {
+        for (let i = 0; i < Math.floor(time / 5); i++) {
+            rest += "☕️"
+        }
+    } else if (time >= 30) {
+        for (let i = 0; i < Math.floor(time / 10); i++) {
+            rest += "🍱"
+        }
+    }
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} • <span>{rest} {time} min read</span></small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,7 +1,7 @@
 import Article from "./Article"
 
 function ArticleList ({posts}) {
-    const articlePosts = posts.map(article => <Article key={article.id} title={article.title} date={article.date} preview={article.preview}/>)
+    const articlePosts = posts.map(article => <Article key={article.id} title={article.title} date={article.date} preview={article.preview} time={article.minutes}/>)
     return (
         <main>
             {articlePosts}
